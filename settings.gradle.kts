17ac44a8bad449dc3461dea8f58419e6b646c41d
sproshchaev <sproshchaev@gmail.com>
2024-06-19 00:56:21 +0500

    fix: actions.md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,5 @@ rootProject.name = "kafka-for-developers"
 include("webinar-01")
 include("webinar-01:producer-service")
 findProject(":webinar-01:producer-service")?.name = "producer-service"
+include("webinar-01:consumer-service")
+findProject(":webinar-01:consumer-service")?.name = "consumer-service"
