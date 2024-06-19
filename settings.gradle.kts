ac6ba3d762e96ac87a59c8fffd7189f74144c0f6
sproshchaev <sproshchaev@gmail.com>
2024-06-19 15:51:26 +0500

    feat: add webinar-02

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,4 @@ include("webinar-01:producer-service")
 findProject(":webinar-01:producer-service")?.name = "producer-service"
 include("webinar-01:consumer-service")
 findProject(":webinar-01:consumer-service")?.name = "consumer-service"
+include("webinar-02")
