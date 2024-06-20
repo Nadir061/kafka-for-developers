bed73d188bdd3dc9b7f0867557210b07b1fa675b
sproshchaev <sproshchaev@gmail.com>
2024-06-20 23:08:36 +0500

    refact: producer, consumer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,11 @@ findProject(":webinar-01:consumer-service")?.name = "consumer-service"
 include("webinar-02")
 include("webinar-03")
 include("webinar-04")
+include("webinar-02:consumer-service")
+findProject(":webinar-02:consumer-service")?.name = "consumer-service"
+include("webinar-03:consumer-service")
+findProject(":webinar-03:consumer-service")?.name = "consumer-service"
+include("webinar-02:producer-service")
+findProject(":webinar-02:producer-service")?.name = "producer-service"
+include("webinar-03:producer-service")
+findProject(":webinar-03:producer-service")?.name = "producer-service"
