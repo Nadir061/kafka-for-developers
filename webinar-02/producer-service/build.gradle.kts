a611673b0f79f7e7244d80e3ea43f6ce2612c35b
sproshchaev <sproshchaev@gmail.com>
2024-06-21 18:19:16 +0500

    added producer and consumer

--- webinar-02/producer-service/build.gradle.kts
+++ webinar-02/producer-service/build.gradle.kts
@@ -10,8 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.kafka:kafka-clients:3.4.0")
+
+    implementation ("org.slf4j:slf4j-api:2.0.13")
+    implementation ("ch.qos.logback:logback-classic:1.5.6")
+
 }
 
 tasks.test {
