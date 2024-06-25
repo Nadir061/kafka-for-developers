af51996b69cedb962d6858f6d4415924169f7875
sproshchaev <sproshchaev@gmail.com>
2024-06-25 21:34:32 +0500

    feat: add Person stream

--- webinar-06/producer-service/src/main/java/com/prosoft/KafkaProducerApp.java
+++ webinar-06/producer-service/src/main/java/com/prosoft/KafkaProducerApp.java
@@ -1,14 +1,17 @@
 package com.prosoft;
 
 import com.prosoft.config.KafkaConfig;
+import com.prosoft.domain.Person;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
- * Webinar-06: Kafka producer-service (variant #1)
- * Использования метода producer.send(producerRecord) без обработки результата.
+ * Webinar-06: Kafka producer-service (отправка объектов класса Person)
  */
 public class KafkaProducerApp {
 
@@ -16,16 +19,27 @@ public class KafkaProducerApp {
     private static final int MAX_MESSAGE = 10;
 
     public static void main(String[] args) {
-        try (KafkaProducer<String, String> producer = new KafkaProducer<>(KafkaConfig.getProducerConfig())) {
+        try (KafkaProducer<Long, Person> producer = new KafkaProducer<>(KafkaConfig.getProducerConfig())) {
+
             for (int i = 0; i < MAX_MESSAGE; i++) {
-                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(KafkaConfig.TOPIC, "key-" + i, "value-" + i);
+                Person person = createPerson(i);
+
+                /** Конструктор ProducerRecord(topic, key, value) */
+                ProducerRecord<Long, Person> producerRecord = new ProducerRecord<>(KafkaConfig.TOPIC, person.getId(), person);
+
+                /** Отправка сообщения */
                 producer.send(producerRecord);
-                logger.info("Отправлено сообщение: key-{}, value-{}", i, i);
+
+                logger.info("Отправлено сообщение: key-{}, value-{}", person.getId(), person);
             }
-            logger.info("Отправка завершена.");
         } catch (Exception e) {
             logger.error("Ошибка при отправке сообщений в Kafka", e);
         }
+    }
+
+    private static Person createPerson(int index) {
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss"));
+        return new Person(index, "FirstName-" + currentTime, "LastName" + index, 20 + index);
     }
 
 }
