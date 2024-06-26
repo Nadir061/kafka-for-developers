2cc1b834e63502c91dd68ee3578887108e10a9db
sproshchaev <sproshchaev@gmail.com>
2024-06-26 15:28:37 +0500

    feat: join

---
+++ webinar-06/stream-service/src/main/java/com/prosoft/KafkaStream04App.java
@@ -0,0 +1,83 @@
+package com.prosoft;
+
+import com.prosoft.config.KafkaConfig04;
+import com.prosoft.domain.Person;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Properties;
+
+/**
+ * Webinar-06: Kafka stream-service (variant #4)
+ * Чтение данных из KafkaConfig04.OUTPUT_EVEN_AGE_TOPIC и KafkaConfig04.OUTPUT_ODD_AGE_TOPIC и объединение через .outerJoin
+ * в KafkaConfig04.OUTPUT_EVEN_AND_ODD_AGE_TOPIC
+ */
+public class KafkaStream04App {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaStream04App.class);
+
+    public static void main(String[] args) {
+
+        /** Получаем конфигурацию Kafka Streams */
+        Properties config = KafkaConfig04.getStreamsConfig();
+
+        /** Создаем StreamsBuilder */
+        StreamsBuilder builder = new StreamsBuilder();
+
+        /** Читаем данные из топиков с четным и нечетным возрастом */
+        KStream<Long, Person> evenAgeInputStream = builder.stream(KafkaConfig04.OUTPUT_EVEN_AGE_TOPIC);
+        KStream<Long, Person> oddAgeInputStream = builder.stream(KafkaConfig04.OUTPUT_ODD_AGE_TOPIC);
+
+        /** Выполняем Join потоков */
+        KStream<Long, String> joinedStream = evenAgeInputStream.outerJoin(oddAgeInputStream,
+                (evenPerson, oddPerson) -> {
+                    if (evenPerson != null && oddPerson != null) {
+                        return "Even: " + evenPerson.toString() + ", Odd: " + oddPerson.toString();
+                    } else if (evenPerson != null) {
+                        return "Even: " + evenPerson.toString();
+                    } else if (oddPerson != null) {
+                        return "Odd: " + oddPerson.toString();
+                    } else {
+                        return "No data";
+                    }
+                },
+                JoinWindows.of(Duration.ofMinutes(5)));
+
+        /** Отправляем результат Join в новый топик */
+        joinedStream.to(KafkaConfig04.OUTPUT_EVEN_AND_ODD_AGE_TOPIC, Produced.with(Serdes.Long(), Serdes.String()));
+
+        /** Выводим результат Join в консоль  */
+        joinedStream.print(Printed.<Long, String>toSysOut().withLabel("Joined Result"));
+
+        /** Создание топологии из builder */
+        Topology topology = builder.build();
+        logger.info("Топология:\n{}", topology.describe());
+
+        /** Использование try-with-resources для автоматического закрытия KafkaStreams */
+        try (KafkaStreams streams = new KafkaStreams(builder.build(), config)) {
+
+            /** Запуск приложения Kafka Streams */
+            streams.start();
+
+            /** Добавление хука (Shutdown hook - выполнение кода при завершении работы Java Virtual Machine (JVM) */
+            Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+            /** Блокировка главного потока, для того чтобы приложение оставалось активным после запуска KafkaStreams */
+            Thread.sleep(Long.MAX_VALUE);
+
+        } catch (Exception e) {
+            logger.error("Произошла ошибка при работе Kafka Streams", e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
---
+++ webinar-06/stream-service/src/main/java/com/prosoft/config/KafkaConfig04.java
@@ -0,0 +1,43 @@
+package com.prosoft.config;
+
+import com.prosoft.serde.PersonSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
+/**
+ * Webinar-06: KafkaConfig для KafkaStream02App содержит конфигурацию для kafka-streams в виде метода getStreamsConfig().
+ */
+public class KafkaConfig04 {
+
+    /** Топик для Person c четным возрастом */
+    public static final String OUTPUT_EVEN_AGE_TOPIC = "w06-topic-out-even-age";
+    /** Топик для Person c нечетным возрастом */
+    public static final String OUTPUT_ODD_AGE_TOPIC = "w06-topic-out-odd-age";
+
+    /** Топик для Person c объединением через Join четного и нечетного возраста */
+    public static final String OUTPUT_EVEN_AND_ODD_AGE_TOPIC = "w06-topic-out-even-and-odd-age";
+
+    private KafkaConfig04() {
+    }
+
+    public static Properties getStreamsConfig() {
+        Properties properties = new Properties();
+
+        /** Ключ конфигурации в Kafka Streams, который указывает уникальный идентификатор для приложения Kafka Streams */
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-transformer");
+
+        /** Адреса Kafka брокеров */
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+
+        /** Класс Сериализации/Десериализации (Serde) для ключей */
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
+
+        /** Класс Сериализации/Десериализации (Serde) для значений */
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, PersonSerde.class.getName());
+
+        return properties;
+    }
+
+}
