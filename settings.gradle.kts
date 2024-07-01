bbdaed8d3790a2428459b1d55df35b1256423509
sproshchaev <sproshchaev@gmail.com>
2024-07-01 23:46:51 +0500

    feat: add w-01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "kafka-for-developers"
+
+include("webinar-01")
+include("webinar-01:producer-service")
+include("webinar-01:consumer-service")
+findProject(":webinar-01:producer-service")?.name = "producer-service"
+findProject(":webinar-01:consumer-service")?.name = "consumer-service"
+
+include("webinar-02")
+include("webinar-02:producer-service")
+include("webinar-02:consumer-service")
+findProject(":webinar-02:producer-service")?.name = "producer-service"
+findProject(":webinar-02:consumer-service")?.name = "consumer-service"
